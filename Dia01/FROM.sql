c9f3172dee6d6c9101be216c6584753bc8ce137c
ssoaresr <ssoaresr>
2022-12-14 00:01:19 +0000

    Commit

---
+++ Dia01/FROM.sql
@@ -0,0 +1,17 @@
+-- Databricks notebook source
+SELECT * FROM silver_olist.pedido
+
+-- COMMAND ----------
+
+SELECT idPedido, 
+       descSituacao 
+FROM silver_olist.pedido
+
+-- COMMAND ----------
+
+SELECT idPedido, 
+       descSituacao 
+       
+FROM silver_olist.pedido
+
+LIMIT 5
---
+++ Dia01/select.sql
@@ -0,0 +1,10 @@
+-- Databricks notebook source
+SELECT 'OLÁ MUNDO, AQUI QUEM FALA É DA TERRA!!!'
+
+-- COMMAND ----------
+
+SELECT 1 + 10 * 2 
+
+-- COMMAND ----------
+
+
